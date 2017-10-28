6cc727360b2b5766f5245dba7a42ac23c2f45627
hardtobelieve <tuanit96@gmail.com>
2017-10-28 21:51:24 +0700

    remove link

--- pages/index.php
+++ pages/index.php
@@ -25,7 +25,7 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>Material Design Bootstrap</title>
         <!-- Font Awesome -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="bootstrap/font-awesome.min.css">
         <!-- Bootstrap core CSS -->
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <!-- Material Design Bootstrap -->
